54ee0a3e7b2faea98a17e5f21d73c16f3fcfb326
Djanilson Martins <djanlm@hotmail.com>
2021-03-10 22:39:47 -0300

    all tests passed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
+//const { response } = require('express');
 
 const app = express();
 app.use(express.json());
@@ -10,20 +11,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userFound = users.find((user) => user.username === username);
+
+  if (!userFound){
+    return response.status(404).json({error: "user not found!"});
+  }
+
+  request.user = userFound;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10 ){
+    return response.status(403).json({error: "the user reached the limit of 10 tasks"});
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const { id } = request.params;
+
+  const userFound = users.find((user) => user.username === username);
+
+  if (!userFound){
+    return response.status(404).json({error: "user not found!"});
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({error: "invalid to do id."});
+  }
+
+  const todoFound = userFound.todos.find((todo)=> todo.id === id);
+
+  if(!todoFound){
+    return response.status(404).json({error: "task not found."});
+  }
+
+  request.user = userFound;
+  request.todo = todoFound;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const foundUser = users.find((user)=> user.id === id);
+
+  if (!foundUser){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user=foundUser;
+
+  return next();
 }
+
+
+
+
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
@@ -110,7 +163,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
   const { user, todo } = request;
 
   const todoIndex = user.todos.indexOf(todo);
-
+  console.log(todoIndex);
   if (todoIndex === -1) {
     return response.status(404).json({ error: 'Todo not found' });
   }
